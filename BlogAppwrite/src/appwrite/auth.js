698079fc8dd81acd5e572710b0e2b5ecfa1d7b8c
Khushal-Kumar <146096972+Khushal-Kumar@users.noreply.github.com>
2024-05-06 10:48:29 +0000

    Build Authentication Service

--- BlogAppwrite/src/appwrite/auth.js
+++ BlogAppwrite/src/appwrite/auth.js
@@ -11,6 +11,45 @@ export class AuthService {
             .setProject(config.appwrite_ProjectId);
         this.account = new Account(this.client);
     }
+
+    async createAccount({email, password, name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(),email,password);
+            if(userAccount) {
+            //login
+            }
+            else {
+                return userAccount;
+            }
+        } catch (error) {
+            console.log("Appwrite :: createAccount :: error ", error);
+        }
+    }
+
+    async login({email, password}){
+        try {
+            return this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            console.log("Appwrite :: login :: error ",error);
+        }
+    }
+
+    async getLoginInfo(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite :: getLoginInfo :: error ",error);
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite :: logout :: error ",error);
+        }
+    }
 };
 
 const authService = new AuthService();
