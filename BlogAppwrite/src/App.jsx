4207a8bc973c8c0c6ec560ae7ec7f20a9f43e424
Khushal-Kumar <146096972+Khushal-Kumar@users.noreply.github.com>
2024-05-06 12:30:42 +0000

    22 Videos Completed @chai-aur-react

--- BlogAppwrite/src/App.jsx
+++ BlogAppwrite/src/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react"
+import authService from "./appwrite/auth";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
+
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  const [loading , setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getLoginInfo()
+      .then((userData)=> {
+        if(userData){
+          dispatch(login(userData))
+        } else{
+          dispatch(logout())
+        }
+      })
+      .finally(()=> setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className="mx-auto bg-black text-white w-1/2 m-60 p-5 text-3xl">Loading...</div>
+  ):null
 }
